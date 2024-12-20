3baef3b56a107456006a2bd42bd007ab8ea10fa2
zhouzj666 <15880553069@163.com>
2024-12-21 02:39:45 +0800

    Update raft.go

--- NJU-Dis/raft/raft.go
+++ NJU-Dis/raft/raft.go
@@ -10,27 +10,24 @@ import (
 )
 
 const (
-	FOLLOWER  = 0 
-	CANDIDATE = 1 
-	LEADER    = 2 
+	FOLLOWER  = 0 // 跟随者状态
+	CANDIDATE = 1 // 候选者状态
+	LEADER    = 2 // 领导者状态
 )
 
 const (
-	electionTimeoutMin = 300 * time.Millisecond 
-	electionTimeoutMax = 600 * time.Millisecond 
-	heartbeatTimeout   = 50 * time.Millisecond  
-	checkTimeout       = 5 * time.Millisecond   
+	electionTimeoutMin = 300 * time.Millisecond // 选举超时最小值
+	electionTimeoutMax = 600 * time.Millisecond // 选举超时最大值
+	heartbeatTimeout   = 50 * time.Millisecond  // 心跳超时时间
+	checkTimeout       = 5 * time.Millisecond   // 检查超时时间
 )
 
 // 生成随机选举超时时间
 func randTimeout() time.Duration {
-	diff := (electionTimeoutMax - electionTimeoutMin).Milliseconds()
-	return electionTimeoutMin + time.Duration(rand.Intn(int(diff)))*time.Millisecond
+	return electionTimeoutMin + time.Duration(rand.Intn(int(electionTimeoutMax-electionTimeoutMin)))*time.Millisecond
 }
 
 // ApplyMsg 用于向上层应用传递的消息
-// 包括日志索引、命令和快照数据
-
 type ApplyMsg struct {
 	Index       int
 	Command     interface{}
@@ -38,31 +35,31 @@ type ApplyMsg struct {
 	Snapshot    []byte
 }
 
-
+// LogItem 表示日志条目
 type LogItem struct {
-	Term    int
-	Command interface{}
+	Term    int         // 任期
+	Command interface{} // 命令
 }
 
-
+// Raft 结构体表示一个 Raft 节点
 type Raft struct {
-	mu        sync.Mutex           
-	peers     []*labrpc.ClientEnd  
-	persister *Persister           
-	me        int                  
+	mu        sync.Mutex           // 互斥锁，保护共享状态
+	peers     []*labrpc.ClientEnd  // 其他节点的 RPC 客户端
+	persister *Persister           // 持久化存储
+	me        int                  // 当前节点的 ID
 
-	currentTerm int   
-	votedFor    int   
-	log         []LogItem
+	currentTerm int       // 当前任期
+	votedFor    int       // 当前任期内投票给的候选者 ID
+	log         []LogItem // 日志条目
 
-	commitIndex int 
-	lastApplied int 
+	commitIndex int // 已提交的日志索引
+	lastApplied int // 已应用到状态机的日志索引
 
-	nextIndex  []int 
-	matchIndex []int 
+	nextIndex  []int // 每个节点的下一个日志条目索引
+	matchIndex []int // 每个节点已复制的日志条目索引
 
-	role  int  // 当前角色 (FOLLOWER, CANDIDATE, LEADER)
-	votes int  // 获得的选票数
+	role  int         // 当前角色 (FOLLOWER, CANDIDATE, LEADER)
+	votes int         // 获得的选票数
 	timer *time.Timer // 定时器，用于选举或心跳
 }
 
@@ -102,7 +99,6 @@ func (rf *Raft) readPersist(data []byte) {
 }
 
 // resetToFollower 将当前节点重置为跟随者状态
-// 并更新任期信息
 func (rf *Raft) resetToFollower(term int) {
 	if term > rf.currentTerm {
 		rf.currentTerm = term
@@ -114,19 +110,17 @@ func (rf *Raft) resetToFollower(term int) {
 }
 
 // RequestVoteArgs 请求投票的参数
-// 包括候选人的任期、ID、最后日志索引和任期
 type RequestVoteArgs struct {
-	Term         int
-	CandidateId  int
-	LastLogIndex int
-	LastLogTerm  int
+	Term         int // 候选人的任期
+	CandidateId  int // 候选人的 ID
+	LastLogIndex int // 候选人最后日志条目的索引
+	LastLogTerm  int // 候选人最后日志条目的任期
 }
 
 // RequestVoteReply 请求投票的回复
-// 包括当前任期和是否授予投票
 type RequestVoteReply struct {
-	Term        int
-	VoteGranted bool
+	Term        int  // 当前任期
+	VoteGranted bool // 是否授予投票
 }
 
 // RequestVote 处理投票请求
@@ -153,21 +147,19 @@ func (rf *Raft) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) {
 }
 
 // AppendEntriesArgs 附加日志请求的参数
-// 包括任期、领导者信息、前日志索引和条目
 type AppendEntriesArgs struct {
-	Term         int
-	LeaderId     int
-	PrevLogIndex int
-	PrevLogTerm  int
-	Entries      []LogItem
-	LeaderCommit int
+	Term         int       // 领导者的任期
+	LeaderId     int       // 领导者的 ID
+	PrevLogIndex int       // 前一个日志条目的索引
+	PrevLogTerm  int       // 前一个日志条目的任期
+	Entries      []LogItem // 要附加的日志条目
+	LeaderCommit int       // 领导者的已提交日志索引
 }
 
 // AppendEntriesReply 附加日志请求的回复
-// 包括当前任期和是否成功
 type AppendEntriesReply struct {
-	Term    int
-	Success bool
+	Term    int  // 当前任期
+	Success bool // 是否成功
 }
 
 // AppendEntries 处理附加日志请求
@@ -176,13 +168,15 @@ func (rf *Raft) AppendEntries(args AppendEntriesArgs, reply *AppendEntriesReply)
 	defer rf.unlock()
 
 	reply.Term = rf.currentTerm
+	reply.Success = false
+
 	if args.Term < rf.currentTerm {
 		return
 	}
 
 	rf.resetToFollower(args.Term)
 
-	if args.PrevLogIndex >= len(rf.log) || rf.log[args.PrevLogIndex].Term != args.PrevLogTerm {
+	if args.PrevLogIndex >= len(rf.log) || (args.PrevLogIndex >= 0 && rf.log[args.PrevLogIndex].Term != args.PrevLogTerm) {
 		return
 	}
 
@@ -207,7 +201,6 @@ func min(a, b int) int {
 }
 
 // Start 开始一个新的命令
-// 返回命令的索引、任期和是否为领导者
 func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	rf.lock()
 	defer rf.unlock()
@@ -258,7 +251,116 @@ func Make(peers []*labrpc.ClientEnd, me int, persister *Persister, applyCh chan 
 func (rf *Raft) runElectionTimer() {
 	for rf.me != -1 {
 		<-rf.timer.C
-		// 选举逻辑简化处理
+		rf.lock()
+		if rf.role != LEADER {
+			rf.startElection()
+		}
+		rf.unlock()
+	}
+}
+
+// startElection 发起选举
+func (rf *Raft) startElection() {
+	rf.currentTerm++
+	rf.role = CANDIDATE
+	rf.votedFor = rf.me
+	rf.votes = 1
+	rf.persist()
+	rf.timer.Reset(randTimeout())
+
+	lastLogIndex := len(rf.log) - 1
+	lastLogTerm := rf.log[lastLogIndex].Term
+
+	for i := range rf.peers {
+		if i != rf.me {
+			go rf.sendRequestVote(i, &RequestVoteArgs{
+				Term:         rf.currentTerm,
+				CandidateId:  rf.me,
+				LastLogIndex: lastLogIndex,
+				LastLogTerm:  lastLogTerm,
+			})
+		}
+	}
+}
+
+// sendRequestVote 发送投票请求
+func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs) {
+	var reply RequestVoteReply
+	if rf.peers[server].Call("Raft.RequestVote", args, &reply) {
+		rf.lock()
+		defer rf.unlock()
+
+		if rf.role != CANDIDATE || rf.currentTerm != args.Term {
+			return
+		}
+
+		if reply.Term > rf.currentTerm {
+			rf.resetToFollower(reply.Term)
+			return
+		}
+
+		if reply.VoteGranted {
+			rf.votes++
+			if rf.votes > len(rf.peers)/2 {
+				rf.role = LEADER
+				rf.timer.Reset(heartbeatTimeout)
+				rf.initializeLeaderState()
+			}
+		}
+	}
+}
+
+// initializeLeaderState 初始化领导者状态
+func (rf *Raft) initializeLeaderState() {
+	lastLogIndex := len(rf.log)
+	for i := range rf.peers {
+		rf.nextIndex[i] = lastLogIndex
+		rf.matchIndex[i] = 0
+	}
+	go rf.sendHeartbeats()
+}
+
+// sendHeartbeats 发送心跳
+func (rf *Raft) sendHeartbeats() {
+	for rf.me != -1 && rf.role == LEADER {
+		for i := range rf.peers {
+			if i != rf.me {
+				go rf.sendAppendEntries(i, &AppendEntriesArgs{
+					Term:         rf.currentTerm,
+					LeaderId:     rf.me,
+					PrevLogIndex: rf.nextIndex[i] - 1,
+					PrevLogTerm:  rf.log[rf.nextIndex[i]-1].Term,
+					Entries:      nil,
+					LeaderCommit: rf.commitIndex,
+				})
+			}
+		}
+		time.Sleep(heartbeatTimeout)
+	}
+}
+
+// sendAppendEntries 发送附加日志请求
+func (rf *Raft) sendAppendEntries(server int, args *AppendEntriesArgs) {
+	var reply AppendEntriesReply
+	if rf.peers[server].Call("Raft.AppendEntries", args, &reply) {
+		rf.lock()
+		defer rf.unlock()
+
+		if rf.role != LEADER || rf.currentTerm != args.Term {
+			return
+		}
+
+		if reply.Term > rf.currentTerm {
+			rf.resetToFollower(reply.Term)
+			return
+		}
+
+		if reply.Success {
+			rf.nextIndex[server] = args.PrevLogIndex + len(args.Entries) + 1
+			rf.matchIndex[server] = args.PrevLogIndex + len(args.Entries)
+		} else {
+			rf.nextIndex[server]--
+		}
 	}
 }
 
